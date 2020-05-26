0e599dfb66c399170d9249970d645a1b54dd8068
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2020-05-26 20:09:20 +0200

    aggiunto site per privacy e policy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as song from './song';
+import * as sites from './sites';
 
 // Addeding to firebase functions
 exports.onWriteSong = song.onWriteSong;
 exports.incrementView = song.incrementView;
+exports.privacyAndPolicy = sites.privacyAndPolicy;
